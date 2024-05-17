1a5cdb2adc5e6813d1ee047fa9bc0909fc397697
Luis Felipe de Almeida Nascimento <luis.felipe.almeida.nascimento@gmail.com>
2024-05-16 23:51:48 -0300

    add paragraph

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,8 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
+  
+  <p>Deployed using github actions</p>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
